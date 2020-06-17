503af8e10e1ff7da3feccfa54db1ffd33224e992
fuston05 <fuston05@yahoo.com>
2020-06-17 17:47:15 -0400

    passes moving_zeros.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,12 +4,20 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # check each num is == 0
+    resArr= []
+    for i in range(0, len(arr)):
+        if arr[i] == 0:
+            resArr.append(0)
+        else:
+            resArr.insert(0, arr[i])
 
-    pass
+    return resArr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+    arr1 = [0, 0, 0, 0, 3, 2, 1]
 
-    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr1)}")
